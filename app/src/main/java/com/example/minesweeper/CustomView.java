750165c0d5bb1c8b8da566e63c7f4f046ee84c08
Daniel Fallon <36534105+Danfallon98@users.noreply.github.com>
2020-12-13 16:24:11 +0000

    Milestone 1

---
+++ app/src/main/java/com/example/minesweeper/CustomView.java
@@ -0,0 +1,15 @@
+package com.example.minesweeper;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
+import android.view.View;
+import android.os.Bundle;
+
+public class CustomView extends View {
+
+    public CustomView(Context c){
+        super(c);
+
+    }
+
+}
